2ff370d95ba7f7cdf4c6bdb1811ec3ea5e249d43
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-03-25 17:05:46 +0200

    looks good

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -14,17 +14,17 @@
                             <h5>{{$book->title}}</h5>
                             {{$book->bookAuthor->name}} {{$book->bookAuthor->surname}}
                         </div>
-<div class="list-line__buttons">
-                        <div class="form-group">
-                            <a class="btn btn-outline-secondary btn-sm" href="{{route('book.edit',[$book])}}">EDIT</a>
-                        </div>
-                        <form method="POST" action="{{route('book.destroy', [$book])}}">
-                            @csrf
-                            <button class="btn btn-outline-danger btn-sm" type="submit">DELETE</button>
-                        </form>
                         <div class="list-line__buttons">
-                        </li>
-                        @endforeach
+                            <div class="form-group">
+                                <a class="btn btn-outline-secondary btn-sm" href="{{route('book.edit',[$book])}}">EDIT</a>
+                            </div>
+                            <form method="POST" action="{{route('book.destroy', [$book])}}">
+                                @csrf
+                                <button class="btn btn-outline-danger btn-sm" type="submit">DELETE</button>
+                            </form>
+                            <div class="list-line__buttons">
+                    </li>
+                    @endforeach
                 </div>
             </div>
         </div>
